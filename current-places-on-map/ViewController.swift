766a2838ad0e5460be6a76d8188680632d53897e
William French <wfrench@google.com>
2016-11-23 09:45:43 -0800

    Initial Commit

---
+++ current-places-on-map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  current-places-on-map
+//
+//  Created by William French on 11/23/16.
+//  Copyright © 2016 William French. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
