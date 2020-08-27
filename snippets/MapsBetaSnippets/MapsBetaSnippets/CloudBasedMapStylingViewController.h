cec612d208fc7c8b4f31b762e8b303fe1e675346
Chris Arriola <carriola@google.com>
2020-08-27 10:20:45 -0700

    feat: Add MapsBeta project.

---
+++ snippets/MapsBetaSnippets/MapsBetaSnippets/CloudBasedMapStylingViewController.h
@@ -0,0 +1,17 @@
+//
+//  CloudBasedMapStylingViewController.h
+//  MapsBetaSnippets
+//
+//  Created by Chris Arriola on 8/27/20.
+//  Copyright © 2020 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CloudBasedMapStylingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ snippets/MapsBetaSnippets/MapsBetaSnippets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapsBetaSnippets
+//
+//  Created by Chris Arriola on 8/27/20.
+//  Copyright © 2020 Google. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
