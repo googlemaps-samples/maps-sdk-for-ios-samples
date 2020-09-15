3f883995a87052394f0c3e8730b58427dfb9115b
Chris Arriola <carriola@google.com>
2020-09-14 17:33:46 -0700

    chore: Adding MapsSnippets project.

---
+++ snippets/MapsSnippets/MapsSnippets/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MapsSnippets
+//
+//  Created by Chris Arriola on 9/14/20.
+//  Copyright © 2020 Google. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+
+@end
