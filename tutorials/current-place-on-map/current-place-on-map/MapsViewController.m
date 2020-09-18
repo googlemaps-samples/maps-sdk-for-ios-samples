fdfbf1a027ef92450e5abcdb2e60c6cbc4e6cb04
Chris Arriola <carriola@google.com>
2020-09-18 12:00:37 -0700

    Adding CLLocationManagerDelegate

--- tutorials/current-place-on-map/current-place-on-map/MapsViewController.m
+++ tutorials/current-place-on-map/current-place-on-map/MapsViewController.m
@@ -11,34 +11,99 @@
 @import GooglePlaces;
 @import GoogleMaps;
 
-@interface MapsViewController ()
+@interface MapsViewController () <CLLocationManagerDelegate>
 
 @end
 
 @implementation MapsViewController {
   // [START maps_ios_current_place_declare_params]
   CLLocationManager *locationManager;
-  CLLocation *currentLocation;
+  CLLocation * _Nullable currentLocation;
   GMSMapView *mapView;
   GMSPlacesClient *placesClient;
   float zoomLevel;
   // [END maps_ios_current_place_declare_params]
+  
+  // [START maps_ios_current_place_places_params]
+  // An array to hold the list of likely places.
+  NSArray<GMSPlace *> *likelyPlaces;
+
+  // The currently selected place.
+  GMSPlace * _Nullable selectedPlace;
+  // [END maps_ios_current_place_places_params]
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   zoomLevel = 15.0;
-    // Do any additional setup after loading the view.
+
+  // [START maps_ios_current_place_init_params]
+  // Initialize the location manager.
+  locationManager = [[CLLocationManager alloc] init];
+  locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  [locationManager requestAlwaysAuthorization];
+  locationManager.distanceFilter = 50;
+  [locationManager startUpdatingLocation];
+  locationManager.delegate = self;
+
+  placesClient = [GMSPlacesClient sharedClient];
+  // [END maps_ios_current_place_init_params]
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) listLikelyPlaces
+{
+  // TODO
 }
-*/
+
+// [START maps_ios_current_place_location_manager_delegate]
+// Delegates to handle events for the location manager.
+#pragma mark - CLLocationManagerDelegate
+
+// Handle incoming location events.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+  CLLocation *location = locations.lastObject;
+  NSLog(@"Location: %@", location);
+  
+  GMSCameraPosition * camera = [GMSCameraPosition cameraWithLatitude:location.coordinate.latitude
+                                                           longitude:location.coordinate.longitude
+                                                                zoom:zoomLevel];
+  
+  if (mapView.isHidden) {
+    mapView.hidden = NO;
+    mapView.camera = camera;
+  } else {
+    [mapView animateToCameraPosition:camera];
+  }
+
+  [self listLikelyPlaces];
+}
+
+// Handle authorization for the location manager.
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+  switch (status) {
+    case kCLAuthorizationStatusRestricted:
+      NSLog(@"Location access was restricted.");
+      break;
+    case kCLAuthorizationStatusDenied:
+      NSLog(@"User denied access to location.");
+      // Display the map using the default location.
+      mapView.hidden = NO;
+    case kCLAuthorizationStatusNotDetermined:
+      NSLog(@"Location status not determined.");
+    case kCLAuthorizationStatusAuthorizedAlways:
+    case kCLAuthorizationStatusAuthorizedWhenInUse:
+      NSLog(@"Location status is OK.");
+  }
+}
+
+// Handle location manager errors.
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+  [manager stopUpdatingLocation];
+  NSLog(@"Error: %@", error.localizedDescription);
+}
+// [END maps_ios_current_place_location_manager_delegate]
 
 @end
