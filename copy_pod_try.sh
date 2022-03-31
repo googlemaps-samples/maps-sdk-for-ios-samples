c8b4011d00cc90a326095211fa0d614252778c01
Chris Arriola <carriola@google.com>
2022-03-31 13:20:59 -0700

    chore: Update pod_try_sync.yml (#133)
    * chore: Update pod_try_sync.yml
    
    Change-Id: Ibd53d744c8607ac5e4f8619883888c77d9271afb
    
    * Add verbose
    
    Change-Id: Iffc96d8ab2ac4a557652f1bd16b1d09acc9c1dc2

--- copy_pod_try.sh
+++ copy_pod_try.sh
@@ -18,6 +18,9 @@ DESTINATION_DIR="$2"
 POD_TRY_OPTION="$3"
 
 echo "Invoking 'pod try $POD_NAME'"
+
+pod _1.6.1_ --verbose try $POD_NAME <<< $POD_TRY_OPTION
+
 POD_TRY_DIR="$(pod _1.6.1_ try $POD_NAME <<< $POD_TRY_OPTION | tail -1 | awk '{print $2}' | echo "/$(cut -d\/ -f2-11)")"
 
 echo "Copying contents of '$POD_TRY_DIR' into '$DESTINATION_DIR'"
