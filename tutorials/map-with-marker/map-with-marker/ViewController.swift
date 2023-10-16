f906135592aa45d5abfe428bdc8c1945c4b2c297
Owen LaRosa <owen.larosa@gmail.com>
2023-10-16 08:39:02 -0700

    style: Fix formatting in map with marker snippets

--- tutorials/map-with-marker/map-with-marker/ViewController.swift
+++ tutorials/map-with-marker/map-with-marker/ViewController.swift
@@ -18,22 +18,22 @@ import UIKit
 import GoogleMaps
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        self.view.addSubview(mapView)
-
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.2, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
+        view.addSubview(mapView)
+        
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.2)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
-  }
+    }
 }
+
 // [END maps_ios_map_with_marker]
